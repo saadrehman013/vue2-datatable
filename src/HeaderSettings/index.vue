27543c9373cc3ee7a38d1bb6b1d5c8309b041113
Saad <saad.sami@vservices.com>
2018-07-12 10:44:47 +0500

    headerSettignsBS4Update

--- src/HeaderSettings/index.vue
+++ src/HeaderSettings/index.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="btn-group" name="HeaderSettings">
-    <button class="btn btn-default dropdown-toggle" ref="dropdownBtn" type="button">
+    <button class="btn btn-default dropdown-toggle" ref="dropdownBtn" type="button" data-toggle="dropdown">
       <i class="fa" :class="[usingBak && 'text-info', processingCls || 'fa-cog']"></i>
       <span class="caret"></span>
     </button>
     <div class="dropdown-menu clearfix" :style="drpMenuStyle">
       <div class="-col-group-container">
-        <column-group v-for="(columns, groupName) in colGroups"
-          ref="colGroups" :key="groupName"
-          :group-name="groupName" :columns="columns">
+        <column-group v-for="(columns, groupName) in colGroups" ref="colGroups" :key="groupName" :group-name="groupName" :columns="columns">
         </column-group>
       </div>
       <div class="clearfix" style="margin: 10px 0">
@@ -23,14 +21,12 @@
             <ul class="dropdown-menu">
               <li @click="apply(true)">
                 <a href="#" @click.prevent>
-                  <i class="fa fa-floppy-o"></i>&nbsp;
-                  {{ $i18nForDatatable('Apply and backup settings to local') }}
+                  <i class="fa fa-floppy-o"></i>&nbsp; {{ $i18nForDatatable('Apply and backup settings to local') }}
                 </a>
               </li>
               <li v-if="usingBak" @click="rmBackup()">
                 <a href="#" @click.prevent>
-                  <i class="fa fa-trash-o text-danger"></i>&nbsp;
-                  {{ $i18nForDatatable('Clear local settings backup and restore') }}
+                  <i class="fa fa-trash-o text-danger"></i>&nbsp; {{ $i18nForDatatable('Clear local settings backup and restore') }}
                 </a>
               </li>
             </ul>
@@ -44,91 +40,99 @@
   </div>
 </template>
 <script>
-import ColumnGroup from './ColumnGroup.vue'
-import groupBy from 'lodash/groupBy'
-import keyGen from '../_utils/keyGen'
-import replaceWith from '../_utils/replaceWith'
-import { parseStr, stringify, saveToLS, rmFromLS, getFromLS } from '../_utils/localstorage'
+import ColumnGroup from "./ColumnGroup.vue";
+import groupBy from "lodash/groupBy";
+import keyGen from "../_utils/keyGen";
+import replaceWith from "../_utils/replaceWith";
+import {
+  parseStr,
+  stringify,
+  saveToLS,
+  rmFromLS,
+  getFromLS
+} from "../_utils/localstorage";
 
 export default {
-  name: 'HeaderSettings',
+  name: "HeaderSettings",
   components: { ColumnGroup },
   props: {
     columns: { type: Array, required: true },
     supportBackup: { type: Boolean, required: true }
   },
-  data () {
-    const origSettings = stringify(this.columns)
+  data() {
+    const origSettings = stringify(this.columns);
     return {
       origSettings,
       usingBak: false, // is using backup
-      processingCls: '',
+      processingCls: "",
       storageKey: this.supportBackup && keyGen(origSettings)
-    }
+    };
   },
-  created () {
-    if (!this.supportBackup) return
+  created() {
+    if (!this.supportBackup) return;
 
-    const backup = getFromLS(this.storageKey)
-    if (!backup) return // no backup found
+    const backup = getFromLS(this.storageKey);
+    if (!backup) return; // no backup found
 
-    replaceWith(this.columns, backup)
-    this.usingBak = true
+    replaceWith(this.columns, backup);
+    this.usingBak = true;
   },
-  mounted () {
+  mounted() {
     // control dropdown manually (refers to http://jsfiddle.net/rj3k550m/3)
-    const $el = $(this.$el)
-    $(this.$refs.dropdownBtn).on('click', this.toggle)
-    $(document).on('click', e => {
-      $(e.target).closest($el).length || $el.removeClass('open')
-    })
+    const $el = $(this.$el);
+    $(this.$refs.dropdownBtn).on("click", this.toggle);
+    $(document).on("click", e => {
+      $(e.target).closest($el).length || $el.removeClass("open");
+    });
   },
   computed: {
-    colGroups () {
+    colGroups() {
       return groupBy(
         this.columns.filter(col => col.label || col.title),
-        'group'
-      )
+        "group"
+      );
     },
-    drpMenuStyle () {
-      const w = Object.keys(this.colGroups).length * 150
+    drpMenuStyle() {
+      const w = Object.keys(this.colGroups).length * 150;
       return {
-        padding: '10px 10px 0',
+        padding: "10px 10px 0",
         width: `${w + 25}px`,
         left: `-${w - 25}px`
-      }
+      };
     }
   },
   methods: {
-    apply (alsoBackup) {
-      this.toggle()
-      this.$refs.colGroups.forEach(colGroup => { colGroup.apply() })
-      alsoBackup && this.$nextTick(this.backup)
+    apply(alsoBackup) {
+      this.toggle();
+      this.$refs.colGroups.forEach(colGroup => {
+        colGroup.apply();
+      });
+      alsoBackup && this.$nextTick(this.backup);
     },
-    backup () {
-      saveToLS(this.storageKey, this.columns)
-      this.showProcessing()
-      this.usingBak = true
+    backup() {
+      saveToLS(this.storageKey, this.columns);
+      this.showProcessing();
+      this.usingBak = true;
     },
-    rmBackup () {
-      rmFromLS(this.storageKey)
-      this.showProcessing()
-      this.usingBak = false
-      
-      replaceWith(this.columns, parseStr(this.origSettings)) // restore
+    rmBackup() {
+      rmFromLS(this.storageKey);
+      this.showProcessing();
+      this.usingBak = false;
+
+      replaceWith(this.columns, parseStr(this.origSettings)); // restore
     },
-    toggle () {
-      $(this.$el).toggleClass('open')
+    toggle() {
+      $(this.$el).toggleClass("open");
     },
-    showProcessing () {
-      ['fa-spinner fa-pulse', 'fa-check', ''].forEach((cls, idx) => {
+    showProcessing() {
+      ["fa-spinner fa-pulse", "fa-check", ""].forEach((cls, idx) => {
         setTimeout(() => {
-          this.processingCls = cls
-        }, idx * 1000)
-      })
+          this.processingCls = cls;
+        }, idx * 1000);
+      });
     }
   }
-}
+};
 </script>
 <style>
 .-col-group-container {
